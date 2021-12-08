115ca676d9682a0df4d89d6761391cb65a90ca4f
vamshirr <vamshi.r@comakeit.com>
2021-12-08 13:11:34 +0530

    sencond

--- src/main/java/com/boto/demopractice/DemopracticeApplication.java
+++ src/main/java/com/boto/demopractice/DemopracticeApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemopracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemopracticeApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemopracticeApplication.class, args);
+    System.out.println();
+
+  }
 
 }
